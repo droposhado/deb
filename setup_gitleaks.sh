9a8db5a16b873fdbc4b4c3ac802af946ca971e07
Flaverton Rodrigues Rosa <sign@droposhado.org>
2022-04-04 11:51:13 -0300

    gitleaks: using correct url and decompressing tar.gz

--- setup_gitleaks.sh
+++ setup_gitleaks.sh
@@ -3,11 +3,14 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-curl -sSL "https://github.com/zricethezav/gitleaks/releases/download/v${VERSION}/gitleaks-linux-${PKG_ARCH}" -o "$NAME"
+curl -sSL
+"https://github.com/zricethezav/gitleaks/releases/download/v${VERSION}/gitleaks-${VERSION}-linux_x64.tar.gz" -o "${NAME}-${VERSION}.tar.gz"
 
-chmod +x "$NAME"
+tar xzf "${NAME}-${VERSION}.tar.gz"
+
+chmod +x "${NAME}"
 
 fpm -s dir -t "$PKG_TYPE" --name "$NAME" --version "$VERSION" \
      --architecture "$PKG_ARCH" --license "MIT" \
      --description "Scan git repos (or files) for secrets using regex and entropy" \
-    "$NAME"=/usr/bin/
+    "${NAME}"=/usr/bin/
