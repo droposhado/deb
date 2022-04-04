ca060bbb7eb76bc1372725be78eb48404b4224b0
Flaverton Rodrigues Rosa <sign@droposhado.org>
2022-04-04 11:01:40 -0300

    force regenerate repo with new gpg sign

---
+++ setup_i3status-rust.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+curl -sSL "https://github.com/greshake/i3status-rust/archive/v${VERSION}.tar.gz" -o "${NAME}-${VERSION}.tar.gz"
+
+tar xzf "${NAME}-${VERSION}.tar.gz"
+
+# Extraction create i3status-rust-$VERSION folder, but needs be "${NAME}-${VERSION}"
+# This command normalize
+mkdir "${NAME}-${VERSION}"
+tar xzf "${NAME}-${VERSION}.tar.gz" -C "${NAME}-${VERSION}" --strip-components 1
+
+cd "${NAME}-${VERSION}" || exit 1
+
+sudo apt-get install build-essential libssl-dev libdbus-1-dev \
+    libnotmuch-dev libpulse-dev libgoogle-perftools-dev
+
+cargo test --all-features --all-targets --verbose
+
+cargo build --release --all-features --all-targets --verbose
+
+# Tests whether the final executable is working or has a build
+# error before packaging
+"target/release/${NAME}" --version
+
+fpm -s dir -t "$PKG_TYPE" --name "$NAME" --version "$VERSION" \
+    --architecture "$PKG_ARCH" --license "GPL-3.0" \
+    --description "Very resourcefriendly and feature-rich replacement for i3status, written in pure Rust" \
+    -d "libnotmuch5" -d "libgoogle-perftools4" \
+    --deb-no-default-config-files \
+    files/=/usr/share/i3status-rust/ \
+    target/release/"$NAME"=/usr/bin/"$NAME"
+
+# Move to default location
+mv "${NAME}_${VERSION}_${PKG_ARCH}.deb" ..
