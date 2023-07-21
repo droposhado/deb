8b9936f6d19af54604f2413cece6a9a6dfe5064e
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-07-21 19:43:03 -0300

    add verbose flag to curl

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl -F package=@"${NAME}_${VERSION}_${PKG_ARCH}.deb" \
-   "https://${FURY_TOKEN}@push.fury.io/${FURY_USERNAME}/"
+   "https://${FURY_TOKEN}@push.fury.io/${FURY_USERNAME}/" -vvv
 
